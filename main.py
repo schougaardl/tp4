d09f4d5aa5d94dc1d2f4aca43a33c79927416457
schougaardl <schougaardl@educ.college-montreal.qc.ca>
2025-11-17 11:53:57 -0500

    commit1

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 Lars schougaard 406 exercice su les classes
 """
 import math
+import random
+
 
 class StringFoo:
     def __init__(self):
@@ -17,13 +19,13 @@ class StringFoo:
 
 
 
-
+"""
 sf = StringFoo()
 
 
 sf.set_string("obligation")
 sf.print_string()
-
+"""
 class rectangle:
     def __init__(self, longueure, largeure):
         self.longueure = longueure
@@ -32,10 +34,11 @@ class rectangle:
     def calcul_aire(self):
         self.aire = self.largeure * self.longueure
         print(self.aire)
-
+"""
 banne = rectangle(3,4)
 
 banne.calcul_aire()
+"""
 
 class cercle:
     def __init__(self, rayon):
@@ -48,9 +51,29 @@ class cercle:
     def calcul_circonférence(self):
         self.circonférence = 2 * math.pi * self.rayon
         print(self.circonférence)
-
+"""
 oven = cercle(6)
 oven.calcul_aire()
 oven.calcul_circonférence()
-
+"""
 class hero:
+    def __init__(self,nom):
+        self.nom = nom
+        self.vie = random.randint(1,10) + random.randint(1,10)
+        self.attaque = random.randint(1,6)
+        self.defense = random.randint(1,6)
+        print(self.nom,self.defense,self.attaque,self.vie)
+    def faire_attaque(self):
+        self.force_attaque = self.attaque + random.randint(1,6)
+        print(self.force_attaque)
+    def damage(self,degats):
+        self.vie += self.defense - degats
+        print(self.vie)
+    def est_vivant(self):
+        if (self.vie > 0) : True
+
+
+bob = hero("bob")
+bob.damage(5)
+bob.est_vivant()
+
